4cb8e74c465251269ecab379881f5edf8264d118
SSTentacleSS <ss_tentacle_ss@zohomail.eu>
2023-02-23 06:05:52 +0300

    Remove tests

--- tests/lib.cxx
+++ tests/lib.cxx
@@ -1,10 +1,10 @@
-#include <catch2/catch.hpp>
-#include <lib.hpp>
+// #include <catch2/catch.hpp>
+// #include <lib.hpp>
 
-TEST_CASE("A simple test that checks the correctness of mathematical operations on numbers for an example") {
-  REQUIRE(plus(2, 2) == 4);
-  REQUIRE(minus(2, 2) == 0);
+// TEST_CASE("A simple test that checks the correctness of mathematical operations on numbers for an example") {
+//   REQUIRE(plus(2, 2) == 4);
+//   REQUIRE(minus(2, 2) == 0);
 
-  REQUIRE(2 + 2 == 4);
-  REQUIRE(2 - 2 == 0);
-}
+//   REQUIRE(2 + 2 == 4);
+//   REQUIRE(2 - 2 == 0);
+// }
